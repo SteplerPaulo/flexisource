878e11675dff92fa87ef0ba68d79f57536ea81fb
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-04 03:12:29 +0800

    Update Carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,6 +3,7 @@ import React from "react";
 function Carousel() {
     return (
         <React.Fragment>
+
             <div class="carousel container">
                 <div class="row">
                     <div class="col-lg-4 col-xs-12">
@@ -19,7 +20,7 @@ function Carousel() {
                         <div class="card">
                             <div class="card-body">
                                 <div class="card-pre-title">About</div>
-                                <h5 class="card-title">Our Story</h5>
+                                <h5 class="card-title">Our Values</h5>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis velit ligula. Pellentesque vel lectus tempor orci bibendum...</p>
                                 <a href="#link" class="btn btn-outline-danger">READ MORE</a>
                             </div>
@@ -29,7 +30,7 @@ function Carousel() {
                         <div class="card">
                             <div class="card-body">
                                 <div class="card-pre-title">About</div>
-                                <h5 class="card-title">Our Story</h5>
+                                <h5 class="card-title">Our People</h5>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis velit ligula. Pellentesque vel lectus tempor orci bibendum...</p>
                                 <a href="#link" class="btn btn-outline-danger">READ MORE</a>
                             </div>
